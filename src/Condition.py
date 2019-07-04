77e22aef737ad47307dbf299e94d27862aa1e658
Hannah Brown <hannahbrown@Hannahs-Mac-mini.local>
2019-07-04 15:50:15 -0700

    fixed function names

--- src/Condition.py
+++ src/Condition.py
@@ -3,31 +3,31 @@ from enum import Enum
 class Condition:
     clauses = list() # this should now be a list of numbers to allow for easier printing
     repeat = False
-    numRepeats = 0
+    num_repeats = 0
     increment = 0
 
-    def __init__(self, clauses = list(), repeat = False,  numRepeats = 0, increment = 0):
+    def __init__(self, clauses = list(), repeat = False,  num_repeats = 0, increment = 0):
         self.clauses = clauses
         self.repeat = repeat
-        self.numRepeats = numRepeats
+        self.num_repeats = num_repeats
         self.increment = increment
 
-    def setClauses(self, clauses):
+    def set_clauses(self, clauses):
         self.clauses = clauses
     
-    def setRepeat(self, repeat):
+    def set_repeat(self, repeat):
         self.repeat = repeat
 
-    def setNumRepeats(self, num):
-        self.numRepeats = num
+    def set_num_repeats(self, num):
+        self.num_repeats = num
 
-    def setIncrement(self, increment):
+    def set_increment(self, increment):
         self.increment = increment
             
-    def addClause(self, clause):
+    def add_clause(self, clause):
         self.clauses.append(clause)
 
-    def writeCondition(self, outFile):
+    def write_condition(self, out_file):
         # print the condition w/ proper repeats
         # Loop over clauses, incrementing w/ first increment
         # then loop over clauses w/ subsequent increments until done
@@ -36,17 +36,17 @@ class Condition:
             for y in x:
                 if y == 0:
                     raise Exception("ERROR: 0 found in a clause")
-                print(y, end=" ", file=outFile)
-            print("0", file=outFile)
+                print(y, end=" ", file=out_file)
+            print("0", file=out_file)
 
         if self.repeat:
-            for x in range(1, self.numRepeats):
+            for x in range(1, self.num_repeats):
                 for y in self.clauses:
                     for z in y:               
                         if z < 0:
-                            print(str(z - x * self.increment), end=" ", file=outFile)
+                            print(str(z - x * self.increment), end=" ", file=out_file)
                         else:
-                            print(str(z + x * self.increment), end=" ", file=outFile)
+                            print(str(z + x * self.increment), end=" ", file=out_file)
 
                                             
-                    print("0", file=outFile)
+                    print("0", file=out_file)
