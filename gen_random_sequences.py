f52ff5238b4b4eacd5341cdcaad9cccb8a444e1d
Hannah Brown <hannahbrown@Hannahs-Mac-mini.local>
2019-09-12 10:36:34 -0700

    added gen_random_sequences and 2/3 1 distribution results up through 13

---
+++ gen_random_sequences.py
@@ -0,0 +1,32 @@
+import random
+import sys
+
+def main(argv):
+    length = int(argv[1])
+    n = int(argv[2])
+    prob = float(argv[3])
+    file_path = argv[4]
+    file_name_base = file_path + "/" + argv[5] if len(argv) == 6 else file_path + "/length" + str(length)
+    a = 0
+    b = 1/prob
+    strings = list()
+
+    while len(strings) < n:
+        next_str = ""
+
+        while len(next_str) < length:
+            if random.uniform(a,b) <= 1:
+                next_str += "0"
+            else:
+                next_str += "1"
+            
+        if next_str not in strings:
+            strings.append(next_str)
+            file_name = file_name_base + "_" + str(len(strings))
+
+            with open(file_name, "w+") as f:
+                print(strings[len(strings) - 1])
+                print(strings[len(strings) - 1], file=f)
+
+main(sys.argv)   
+#main(["gen_random_sequences.py", "5", "5", ".333", "./output", "test"])
